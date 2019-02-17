92a6c1333b5663ed74d46e8f70ce17fc11001319
hidx1 <hidx1@users.github.com>
2019-02-18 03:21:32 +0700

    test

--- src/main/java/com/hangman/bot/bothangman/BotHangmanApplication.java
+++ src/main/java/com/hangman/bot/bothangman/BotHangmanApplication.java
@@ -147,7 +147,7 @@ public class BotHangmanApplication extends SpringBootServletInitializer {
                             String replyToken = messageEvent.getReplyToken();
                             BotMessage(replyToken, "Correct!\nCategory: " + current_cat + ".\nLives: " +
                                     lives + ".\nAnswer: " + answer + ".\nCongratulations! You won the game!" +
-                                    "\n\nType /start to keep playing. Type /stop to stop playing.");
+                                    "\n\nType /start to play again.");
                             game_on = false;
                         } else{
                             String replyToken = messageEvent.getReplyToken();
@@ -160,7 +160,7 @@ public class BotHangmanApplication extends SpringBootServletInitializer {
                             String replyToken = messageEvent.getReplyToken();
                             BotMessage(replyToken, "Too bad, wrong answer.\nCategory: " + current_cat + ".\nLives: " +
                                     lives + ".\nAnswer: " + answer + ".\n\nSorry, you lost :(\nThe answer is " + quiz +
-                                    "\n\nType /start to keep playing. Type /stop to stop playing");
+                                    "\n\nType /start to play again.");
                             game_on = false;
                             initializeGame();
                         } else{
